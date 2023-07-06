986611da05b7b7ec2c7dd9c2469bd2ebb13856f1
Guevara74 <114807542+Guevara74@users.noreply.github.com>
2023-07-06 15:14:11 +0300

    p

--- 2DPlatformer/Assets/Global.cs
+++ 2DPlatformer/Assets/Global.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Global : MonoBehaviour
 {
-    bool timeworks = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +14,6 @@ public class Global : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
