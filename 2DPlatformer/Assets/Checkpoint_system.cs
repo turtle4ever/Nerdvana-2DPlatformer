1bc2307006d1eec463dbffc15f2dbf8d5c6ba1a1
Guevara74 <114807542+Guevara74@users.noreply.github.com>
2023-07-06 15:30:18 +0300

    pp

--- 2DPlatformer/Assets/Checkpoint_system.cs
+++ 2DPlatformer/Assets/Checkpoint_system.cs
@@ -11,7 +11,7 @@ public class Checkpoint_system : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag=="")
+       // if(collision.tag=="")
     }
     // Update is called once per frame
     void Update()
