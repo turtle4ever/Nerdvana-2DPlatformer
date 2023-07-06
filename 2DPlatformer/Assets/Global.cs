3747d9f85544ad36884e0d3c11efd7f50a0c6531
turtle4ever <alex.mihnea.visan@gmail.com>
2023-07-06 07:26:00 +0300

    Update Global.cs

--- 2DPlatformer/Assets/Global.cs
+++ 2DPlatformer/Assets/Global.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Global : MonoBehaviour
 {
-    bool timeworks = false;
+    static bool timeworks = false;
     // Start is called before the first frame update
     void Start()
     {
