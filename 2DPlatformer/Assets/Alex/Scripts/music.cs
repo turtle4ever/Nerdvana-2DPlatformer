bbdb51b0b938e2006b9e41e754567054baa4d860
lozard530 <133408762+lozard530@users.noreply.github.com>
2023-07-07 15:25:49 +0300

    sfx

---
+++ 2DPlatformer/Assets/Alex/Scripts/music.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class music : MonoBehaviour
+{
+    
+    void Start()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
